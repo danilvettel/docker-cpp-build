415b8a7db52cec9a64241c9b4215d83fefe9ef4d
Pratik Chowdhury <pratikc@live.co.uk>
2022-10-06 04:29:30 +0530

    fix(code): OS detection within containers

--- code/build.sh
+++ code/build.sh
@@ -7,12 +7,12 @@ cd /code
 ### Build the code
 g++ main.cxx
 
-
 ### Handling different OSes
-if [  -n "$(uname -a | grep Ubuntu)" ]; then
-    ### Move the generated code to Output directory
-    mv a.out /Output/Ubuntu.exe
-else
+if [ -f /etc/redhat-release ]; then
     ### Move the generated code to Output directory
     mv a.out /Output/CentOS-6.exe
-fi  
+fi
+if [ -f /etc/lsb-release ]; then
+    ### Move the generated code to Output directory
+    mv a.out /Output/Ubuntu.exe
+fi
