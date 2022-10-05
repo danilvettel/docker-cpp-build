f36acc1258f6c00abebab18af152b973e5ad1b0d
Pratik Chowdhury <pratikc@live.co.uk>
2022-10-06 05:19:34 +0530

    fix(code): allow /Code to be read only. So build directly in Output

--- code/build.sh
+++ code/build.sh
@@ -5,14 +5,14 @@ echo "Hello"
 cd /code
 
 ### Build the code
-g++ main.cxx
+g++ main.cxx -o /Output/Release.exe
 
 ### Handling different OSes
 if [ -f /etc/redhat-release ]; then
     ### Move the generated code to Output directory
-    mv a.out /Output/CentOS-6.exe
+    mv /Output/Release.exe /Output/CentOS-6.exe
 fi
 if [ -f /etc/lsb-release ]; then
     ### Move the generated code to Output directory
-    mv a.out /Output/Ubuntu.exe
+    mv /Output/Release.exe /Output/Ubuntu.exe
 fi
